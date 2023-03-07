03ea4606759de2980818e21a8774a62767c6e87f
student <bonniec15@nycstudents.net>
2023-03-07 10:29:31 -0500

    Commit

---
+++ src/Computer.java
@@ -0,0 +1,15 @@
+public class Computer {
+    private int screenSize;
+    private int memory;
+    public Computer(int screenSize, int memory) {
+        this.screenSize = screenSize;
+        this.memory = memory;
+    }
+    //getter methods
+    public int getScreenSize() {
+        return screenSize;
+    }
+    public int getMemory() {
+        return memory;
+    }
+}
